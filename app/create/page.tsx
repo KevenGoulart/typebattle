54e13c582e5a278bb507a2914f04ee1f87f8475b
keven <kevengoulartmm@gmail.com>
2025-12-09 15:08:40 -0300

    updated create page style

--- app/create/page.tsx
+++ app/create/page.tsx
@@ -2,6 +2,7 @@
 
 import { useEffect } from "react";
 import { useRouter } from "next/navigation";
+import { RetroGrid } from "@/components/ui/retro-grid";
 
 function generateRoomCode() {
   return Math.random().toString(36).substring(2, 8).toUpperCase();
@@ -16,8 +17,11 @@ export default function CreateRoomPage() {
   }, [router]);
 
   return (
-    <div className="flex items-center mt-16 text-5xl font-semibold mx-auto w-fit">
-      Criando sala...
+    <div>
+      <RetroGrid />
+      <div className="flex items-center mt-16 text-5xl font-semibold mx-auto w-fit">
+        Criando sala...
+      </div>
     </div>
   );
 }
